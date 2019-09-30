89f5fac4430f225ba3bffadb5d6f7fb2a318cf9f
qamar mudhafar <qamarmudhafar@yahoo.com>
2019-09-30 17:08:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your code here
+Function submitData  (name, email);{
+  const url=http://localhost:3000/users
+const= requestOption {
+  headers:{
+    "Content-Type":
+    "applecation/JSON",
+    "Accept": "applecation/JSON"
+};
+  body: JSON.stringify(name:name , email:email)
+};
+fetch(http://localhost:3000/users, reaquestOption)
